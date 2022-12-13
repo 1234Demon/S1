3a8bbc5e9a6811e0ef5a20a1910fb089ffbe0cd6
Nikolay <chort.krsk@rambler.ru>
2022-12-14 02:18:46 +0700

    new123

--- example002_hellouser/Program.cs
+++ example002_hellouser/Program.cs
@@ -1,4 +1,3 @@
-﻿Console.Write("Введите ваше имя ");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+﻿string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
